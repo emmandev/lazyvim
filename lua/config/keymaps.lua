331399975c23c0a006b23e7a69871bdaec55932b
emmandev <emmandev@users.noreply.github.com>
2025-05-21 03:22:04 +0800

    feat: copy nvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vim-tmux-navigator
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- override "s" binding to use Vim's normal mode "s" operation
+vim.keymap.del("n", "s")
