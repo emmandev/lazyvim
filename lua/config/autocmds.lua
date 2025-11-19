a71831354ea3de12f1ac6b37a548a0c116649264
emmandev <emmandev@users.noreply.github.com>
2025-11-20 07:08:28 +0800

    feat: use spaces for yaml files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Always use spaces for YAML
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "yaml", "yml" },
+  callback = function()
+    vim.opt_local.expandtab = true -- convert <Tab> into spaces
+    vim.opt_local.shiftwidth = 2 -- number of spaces per indent
+    vim.opt_local.softtabstop = 2 -- number of spaces per <Tab>
+    vim.opt_local.tabstop = 2 -- how many spaces a <Tab> looks like
+  end,
+})
